5ecf1c14eba34987c1fd44c0ead33d0940d7fed0
Isaac Alencar <alencarisaac3@gmail.com>
2022-12-28 11:04:55 -0300

    setup database with postgres driver

--- db/migrate/20221228134329_create_todos.rb
+++ db/migrate/20221228134329_create_todos.rb
@@ -3,8 +3,8 @@
 class CreateTodos < ActiveRecord::Migration[7.0]
   def change
     create_table :todos do |t|
-      t.string :title
-      t.boolean :completed
+      t.string :title, presence: true
+      t.boolean :completed, inclusion: { in: [true, false] }
 
       t.timestamps
     end

534c6592655c67b6d8eeaae57047b95cf10d6bc1
Isaac Alencar <alencarisaac3@gmail.com>
2022-12-28 11:47:08 -0300

    add todos model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class Todo < ApplicationRecord
+  before_create :set_defaults
+
+  validates :title, presence: true
+
+  def set_defaults
+    self[:completed] = false
+  end
 end
